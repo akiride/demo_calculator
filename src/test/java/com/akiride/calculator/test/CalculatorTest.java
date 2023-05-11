1ae63efbce7fb6e189f0523e5d03ac301ad8fad1
wing <740548422@qq.com>
2023-05-11 11:13:56 +0800

    新增junit依赖，完善测试用例

--- src/test/java/com/akiride/calculator/test/CalculatorTest.java
+++ src/test/java/com/akiride/calculator/test/CalculatorTest.java
@@ -3,13 +3,77 @@ package com.akiride.calculator.test;
 import com.akiride.calculator.Calculator;
 import com.akiride.calculator.DefaultCalculatorBuilder;
 import com.akiride.calculator.common.OperatorEnum;
-
+import org.junit.Test;
 import java.math.BigDecimal;
 
 public class CalculatorTest {
 
-    public static void main(String[] args) {
-        // TODO if has free time update junit test demo
+    @Test
+    public void addTest() {
+        Calculator simpleCalculator = DefaultCalculatorBuilder.createCalculator();
+        // 4 + 5
+        simpleCalculator.appendNum(new BigDecimal(4));
+        simpleCalculator.appendOperator(OperatorEnum.ADD);
+        simpleCalculator.appendNum(new BigDecimal(5));
+        simpleCalculator.calc();
+        // 9 + 1
+        simpleCalculator.appendOperator(OperatorEnum.ADD);
+        simpleCalculator.appendNum(new BigDecimal(1));
+        simpleCalculator.calc();
+    }
+
+    @Test
+    public void subTest() {
+        Calculator simpleCalculator = DefaultCalculatorBuilder.createCalculator();
+        // 4 - 5
+        simpleCalculator.appendNum(new BigDecimal(4));
+        simpleCalculator.appendOperator(OperatorEnum.SUBTRACT);
+        simpleCalculator.appendNum(new BigDecimal(5));
+        simpleCalculator.calc();
+        // -1 - 1
+        simpleCalculator.appendOperator(OperatorEnum.SUBTRACT);
+        simpleCalculator.appendNum(new BigDecimal(1));
+        simpleCalculator.calc();
+    }
+
+    @Test
+    public void multiTest() {
+        Calculator simpleCalculator = DefaultCalculatorBuilder.createCalculator();
+        // 4 * 5
+        simpleCalculator.appendNum(new BigDecimal(4));
+        simpleCalculator.appendOperator(OperatorEnum.MULTI);
+        simpleCalculator.appendNum(new BigDecimal(5));
+        simpleCalculator.calc();
+        // 20 * 2
+        simpleCalculator.appendOperator(OperatorEnum.MULTI);
+        simpleCalculator.appendNum(new BigDecimal(2));
+        simpleCalculator.calc();
+        // 40 * 0
+        simpleCalculator.appendOperator(OperatorEnum.MULTI);
+        simpleCalculator.appendNum(new BigDecimal(0));
+        simpleCalculator.calc();
+    }
+
+    @Test
+    public void divideTest() {
+        Calculator simpleCalculator = DefaultCalculatorBuilder.createCalculator();
+        // 20 / 5
+        simpleCalculator.appendNum(new BigDecimal(20));
+        simpleCalculator.appendOperator(OperatorEnum.DIVIDE);
+        simpleCalculator.appendNum(new BigDecimal(5));
+        simpleCalculator.calc();
+        // 4 / 8
+        simpleCalculator.appendOperator(OperatorEnum.DIVIDE);
+        simpleCalculator.appendNum(new BigDecimal(8));
+        simpleCalculator.calc();
+        // 0.5 / 0
+        simpleCalculator.appendOperator(OperatorEnum.DIVIDE);
+        simpleCalculator.appendNum(new BigDecimal(0));
+        simpleCalculator.calc();
+    }
+
+    @Test
+    public void undoAndRedoTest() {
         Calculator simpleCalculator = DefaultCalculatorBuilder.createCalculator();
         // 4 * 5
         simpleCalculator.appendNum(new BigDecimal(4));
@@ -29,4 +93,5 @@ public class CalculatorTest {
         // 5
         simpleCalculator.redo();
     }
+
 }
